88e5368a53f4536f36e63a21cb8eb85d729adf96
ghazanKhan33 <ghazan.elitx@gmail.com>
2025-07-08 12:09:22 +0500

    Setting up static files, tailwind etc

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const express = require('express')
+const path = require('path')
 
 const app = express();
 
 app.use(express.json())
 app.use(express.urlencoded({extended: true}))
+app.use(express.static(path.join(__dirname, 'public')))
 app.set('view engine', 'ejs')
 
 app.get('/', function(req, res){
